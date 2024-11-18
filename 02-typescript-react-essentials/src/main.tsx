313806dcd16a0164f0d66f56e6c4b3554bebcc5f
Palak Shah <palak.shah@solvassolutions.com>
2024-11-18 12:17:39 -0500

    useQuery for fetch data

--- 02-typescript-react-essentials/src/main.tsx
+++ 02-typescript-react-essentials/src/main.tsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { Provider } from "react-redux";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
--- 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
+++ 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
@@ -1,9 +1,28 @@
-function Component() {
+import { fetchTours } from "./types";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Component() {
+  const {
+    isPending,
+    isError,
+    error,
+    data: tours
+  } = useQuery({
+    queryKey: ["tours"],
+    queryFn: fetchTours
+  });
+
+  if (isPending) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error: {error.message}</h2>;
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Fetch Data</h2>
+      <h2 className="mb-1">Tours</h2>
+      {tours.map((tour) => (
+        <p className="mb-1" key={tour.id}>
+          {tour.name}
+        </p>
+      ))}
     </div>
   );
 }
-export default Component;
